17e58c5a3191d128d4dd0918ace3f9be38d21058
Context Finance <github@context.finance>
2021-11-28 15:16:50 +0100

    chore: find and replace

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -3,15 +3,13 @@ import { getNetwork } from '@ethersproject/networks'
 import { getDefaultProvider } from '@ethersproject/providers'
 import { TokenAmount } from './entities/fractions/tokenAmount'
 import { Pair } from './entities/pair'
-import IPangolinPair from '@pangolindex/exchange-contracts/artifacts/contracts/pangolin-core/PangolinPair.sol/PangolinPair.json'
+import IContextPair from 'context-exchange-contracts/artifacts/contracts/context-core/ContextPair.sol/ContextPair.json'
 import invariant from 'tiny-invariant'
 import ERC20 from './abis/ERC20.json'
 import { ChainId } from './constants'
 import { Token } from './entities/token'
 
-let TOKEN_DECIMALS_CACHE: { [chainId: number]: { [address: string]: number } } = {
-  
-}
+let TOKEN_DECIMALS_CACHE: { [chainId: number]: { [address: string]: number } } = {}
 
 /**
  * Contains methods for constructing instances of pairs and tokens from on-chain data.
@@ -66,7 +64,7 @@ export abstract class Fetcher {
   ): Promise<Pair> {
     invariant(tokenA.chainId === tokenB.chainId, 'CHAIN_ID')
     const address = Pair.getAddress(tokenA, tokenB, tokenA.chainId)
-    const [reserves0, reserves1] = await new Contract(address, IPangolinPair.abi, provider).getReserves()
+    const [reserves0, reserves1] = await new Contract(address, IContextPair.abi, provider).getReserves()
     const balances = tokenA.sortsBefore(tokenB) ? [reserves0, reserves1] : [reserves1, reserves0]
     return new Pair(new TokenAmount(tokenA, balances[0]), new TokenAmount(tokenB, balances[1]), tokenA.chainId)
   }
